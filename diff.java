cc027f74a35f3ae90a3e3c211a063766877892f1
Ayush Tyagi <ayushtyagi4810poco@gmail.com>
2022-10-04 17:48:52 +0530

    done

---
+++ diff.java
@@ -0,0 +1,9 @@
+public class diff{
+public static void main(String[] args) {
+    int a=99;
+    int b=12;
+
+    int c=a-b;
+    System.out.println(c);
+}
+}
--- sum.java
+++ sum.java
@@ -4,6 +4,6 @@ public class Sum{
     int a=23;
     int b=98;
 
-    System.out.println(a+b);
+    System.out.println("the sum is " +  (a+b));
    } 
 }
